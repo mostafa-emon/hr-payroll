0f920f6a255906a10b34012a914a488a70309f6c
Mostafa Ifty <mostafaifty2@gmail.com>
2020-05-02 13:42:03 +0600

    Supplier Modify

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -1,73 +1,69 @@
 @extends('layouts.master')
 
-    @section('content')
+@section('content')
 
-    <div class="br-mainpanel">
-        <div class="br-pageheader pd-y-15 pd-l-20">
-          <nav class="breadcrumb pd-0 mg-0 tx-12">
-          </nav>
-        </div>
-        @if(session()->has('message'))
-            <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                {{ session()->get('message') }}
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-        @endif
-        <div class="card">
-            <div class="card-header">
-              <div class="row">
-                <div class="col-md-6">
-                  <h3 class="card-title">Supplier</h3>
-                </div>
-                <div class="col-md-6 text-right">
-                  <a class="btn btn-primary btn-sm" href="{{url('supplier/add')}}"><i class="fa fa-plus-circle"></i> Add Supplier</a>
-                </div>
-              </div>
-            </div>
-  
-        <div class="br-pagebody">
-          <div class="br-section-wrapper">
-            <div class="bd bd-gray-300 rounded table-responsive">
-              <table class="table table-striped mg-b-0">
-                <thead>
-                  <tr>
-                    <th>Serial</th>
-                    <th>Name</th>
-                    <th>Cheque Name</th>
-                    <th>Address</th>
-                    <th>Phone</th>
-                    <th>Email</th>
-                    <th>Contact Person</th>
-                    <th>Update</th>
-                    <th>Delete</th>               
-                  </tr>
-                </thead>
-                <tbody>
-                    @foreach($suppliers as $supplier)
-                    <tr>
-                      <td> {{$loop->iteration}} </td>
-                      <td> {{$supplier->name}} </td>
-                      <td> {{$supplier->cheque_name}} </td>
-                      <td> {{$supplier->address}} </td>
-                      <td> {{$supplier->phone}} </td>
-                      <td> {{$supplier->email}} </td>
-                      <td> {{$supplier->contact_person}} </td>
-                      <td>
-                      <a class="btn btn-success btn-sm" href="{{url ('supplier/update/'.$supplier->id) }}"><i class= "fa fa-edit"></i> Update </a>
-                      </td>
-                      <td>
-                      <a class="btn btn-danger btn-sm" href="{{url ('supplier/delete/'.$supplier->id) }}"><i class= "fa fa-minus-circle"></i> Delete</a>
-                      </td>
-                    </tr>
-                    @endforeach
-                  </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+  <div class="br-pageheader pd-y-15 pd-l-20">
+    <nav class="breadcrumb pd-0 mg-0 tx-12">
+      <a class="breadcrumb-item" href="{{ url('/') }}">Home</a>
+      <a class="breadcrumb-item" href="{{ url('supplier') }}">Supplier</a>
+    </nav>
+  </div>
+
+    @if(session()->has('message'))
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        {{ session()->get('message') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    @endif
+
+  <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+    <div style="float:left">
+      <h4 class="tx-gray-800 mg-b-5">Suppliers</h4>
+    </div>
+    <div style="float:right">
+      <a href="{{ url('supplier/add') }}" class="btn btn-primary btn-sm text-white"><i class="fa fa-plus-circle"></i> Add Supplier</a>
+    </div>
+  </div>
+
+  <div class="br-pagebody pd-t-15">
+    <div class="br-section-wrapper">
+      <div class="bd bd-gray-300 rounded table-responsive">
+        <table class="table table-striped mg-b-0">
+          <thead>
+            <tr>
+              <th>Sl</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Phone</th>
+              <th>Email</th>
+              <th>Contact Person</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($suppliers as $supplier)
+              <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $supplier->name }}</td>
+                <td>{{ $supplier->address }}</td>
+                <td>{{ $supplier->phone }}</td>
+                <td>{{ $supplier->email }}</td>
+                <td>{{ $supplier->contact_person }}</td>
+                <td>
+                  <a class="btn btn-success btn-sm" href="{{url ('supplier/update/'.$supplier->id) }}"><i class= "fa fa-edit"></i> Update </a>
+                  </td>
+                  <td>
+                  <a class="btn btn-danger btn-sm" href="{{url ('supplier/delete/'.$supplier->id) }}"><i class= "fa fa-minus-circle"></i> Delete</a>
+                  </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
       </div>
     </div>
-    
-    @endsection
+  </div>
+
+@endsection
--- resources/views/suppliers/update.blade.php
+++ resources/views/suppliers/update.blade.php
@@ -1,66 +1,77 @@
 @extends('layouts.master')
 
-    @section('content')
+@section('content')
 
-      <div class="br-mainpanel">
-          <div class="br-pageheader pd-y-15 pd-l-20">
-            <nav class="breadcrumb pd-0 mg-0 tx-12">
-            </nav>
-          </div>
-          <div class="card">
-            <div class="card-header">
-            <div class="row">
-              <div class="col-md-10">
-              <h2>Update Supplier</h2>
-          
-              <form action="{{ url('supplier/update/'.$suppliers->id) }}" method="POST" class="form-horizontal">
-                {{ csrf_field() }}
-                  <div class="form-group row">
-                    <label for="inputName" class="col-sm-2 col-form-label">Name</label>
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control" name="name" placeholder="Name" value="{{$suppliers->name}}"/>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="inputChequeName" class="col-sm-2 col-form-label">Cheque Name</label>
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control" name="cheque_name" placeholder="Cheque Name" value="{{$suppliers->cheque_name}}"/>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="inputAddress" class="col-sm-2 col-form-label">Address</label>
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control" name="address" placeholder="Address" value="{{$suppliers->address}}"/>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="inputPhone" class="col-sm-2 col-form-label">Phone</label>
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control" name="phone" placeholder="Phone" value="{{$suppliers->phone}}"/>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control" name="email" placeholder="Email" value="{{$suppliers->email}}"/>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <label for="inputContactPerson" class="col-sm-2 col-form-label">Contact Person</label>
-                    <div class="col-sm-10">
-                      <input type="text" class="form-control" name="contact_person" placeholder="Contact Person" value="{{$suppliers->contact_person}}"/>
-                    </div>
-                  </div>
-                  <div class="form-group row">
-                    <div class="offset-sm-2 col-sm-10">
-                      <input type="submit" value="Submit" class="btn btn-danger btn-sm"/>
-                    </div>
-                  </div>
-              </form>
+    <div class="br-pageheader pd-y-15 pd-l-20">
+      <nav class="breadcrumb pd-0 mg-0 tx-12">
+        <a class="breadcrumb-item" href="url{{ url('/') }}">Home</a>
+        <a class="breadcrumb-item" href="url{{ url('/supplier') }}">Suppliers</a>
+        <span class="breadcrumb-item active">Add</span>
+      </nav>
+    </div>
+
+    <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
+      <h4 class="tx-gray-800 mg-b-5">Add Supplier</h4>
+    </div>
+
+    <form action="{{ url('supplier/update/'.$suppliers->id) }}" method="POST">
+      {{ csrf_field() }}
+      <div class="br-pagebody">
+        <div class="br-section-wrapper">
+          <div class="form-layout form-layout-2">
+            <div class="row no-gutters">
+
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label class="form-control-label">Name: <span class="tx-danger">*</span></label>
+                <input class="form-control" type="text" name="name" placeholder="Enter Name" value="{{$suppliers->name}}">
+                </div>
               </div>
-            </div>
-            </div>
-          </div> 
-      </div>
 
-    @endsection
+              <div class="col-md-4 mg-t--1 mg-md-t-0">
+                <div class="form-group mg-md-l--1">
+                  <label class="form-control-label">Email address: <span class="tx-danger">*</span></label>
+                  <input class="form-control" type="text" name="email" placeholder="Enter Email Address" value="{{$suppliers->email}}">
+                </div>
+              </div>
+
+              <div class="col-md-4 mg-t--1 mg-md-t-0">
+                <div class="form-group mg-md-l--1">
+                  <label class="form-control-label">Phone Number: <span class="tx-danger">*</span></label>
+                  <input class="form-control" type="text" name="phone" placeholder="Enter Phone Number" value="{{$suppliers->phone}}">
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label class="form-control-label">Cheque Name: <span class="tx-danger">*</span></label>
+                  <input class="form-control" type="text" name="cheque_name" placeholder="Enter Cheque Name" value="{{$suppliers->cheque_name}}">
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="form-group bd-t-0-force">
+                  <label class="form-control-label">Address: <span class="tx-danger">*</span></label>
+                  <input class="form-control" type="text" name="address" placeholder="Enter Address" value="{{$suppliers->address}}">
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="form-group mg-md-l--1">
+                  <label class="form-control-label">Contact Person: <span class="tx-danger">*</span></label>
+                  <input class="form-control" type="text" name="contact_person" placeholder="Contact Person Name" value="{{$suppliers->contact_person}}">
+                </div>
+              </div>
+
+            </div>
+
+            <div class="form-layout-footer bd pd-20 bd-t-0">
+              <input type="submit" value="Submit" class="btn btn-info"/>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </form>
+
+@endsection
