067a361960841ac47eca888d2c7a3ceb96309a66
Mostafa Emon <mostafaemon.info@gmail.com>
2020-05-03 15:49:15 +0600

    settings start

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -114,7 +114,7 @@
         <ul class="br-menu-sub nav flex-column">
         <li class="nav-item"><a href="{{url('currency')}}" class="nav-link">Currency</a></li>
           <li class="nav-item"><a href="{{url('payment-method')}}" class="nav-link">Payment Method</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">Settings</a></li>
+          <li class="nav-item"><a href="{{url('settings')}}" class="nav-link">Settings</a></li>
         </ul>
       </div>
 
