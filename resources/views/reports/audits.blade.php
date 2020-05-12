caf4dd6f9a45d53d1c7f9fd5c026fa81fda9c9e2
Mostafa Emon <mostafaemon.info@gmail.com>
2020-05-13 02:55:03 +0600

    Configuration Complete

--- resources/views/reports/audits.blade.php
+++ resources/views/reports/audits.blade.php
@@ -45,6 +45,12 @@
                         @if($audit->event == "created") Create Site Office @endif
                         @if($audit->event == "updated") Update Site Office @endif
                         @if($audit->event == "deleted") Delete Site Office @endif
+                    @elseif($audit->auditable_type == "App\Printer")
+                        @if($audit->event == "created") Create Printer @endif
+                        @if($audit->event == "updated") Update Printer @endif
+                        @if($audit->event == "deleted") Delete Printer @endif
+                    @elseif($audit->auditable_type == "App\Setting")
+                        Settings Update
                     @endif
                 </td>
                 <td>
@@ -77,6 +83,47 @@
                             }
                             echo rtrim($old, ', ');
                         }
+
+                        if($audit->auditable_type == "App\Printer"){
+                            $old_value = json_decode($audit->old_values);
+                            $old = "";
+                            foreach($old_value as $key => $value){
+                                if($key == "print_name") {$old = $old."Name: ".$value.', ';}
+                                else if($key == "top") {$old = $old."Top: ".$value.', ';}
+                                else if($key == "left") {$old = $old."Left: ".$value.', ';}
+                                else if($key == "rotate") {$old = $old."Rotate: ".$value.', ';}
+                            }
+                            echo rtrim($old, ', ');
+                        }
+
+                        if($audit->auditable_type == "App\Setting"){
+                            $old_value = json_decode($audit->old_values);
+                            $old = "";
+                            foreach($old_value as $key => $value){
+                                if($key == "mr_number") {
+                                    if($value == "auto") { $old = $old."MR Number: Automatic, "; }
+                                    else { $old = $old."MR Number: Manual, "; }
+                                }
+                                else if($key == "mr_size") {
+                                    if($value == "full_size") { $old = $old."MR Size: Full Page, "; }
+                                    else { $old = $old."MR Size: Half Page, "; }
+                                }
+                                else if($key == "amount_in_word_format") {
+                                    if($value == "crore_lakh_thousand") { $old = $old."Amount in Word Fomrat: Crore Lakh Thousand, "; }
+                                    else if($value == "crore_lac_thousand") { $old = $old."Amount in Word Fomrat: Crore Lac Thousand, "; }
+                                    else if($value == "billion_million_thousand") { $old = $old."Amount in Word Fomrat: Billion Million Thousand, "; }
+                                }
+                                else if($key == "approval_for_mr") {
+                                    if($value == 1) { $old = $old."Approval for MR: Yes, "; }
+                                    else { $old = $old."Approval for MR: No, "; }
+                                }
+                                else if($key == "approval_for_cheque") {
+                                    if($value == 1) { $old = $old."Approval for Cheque: Yes, "; }
+                                    else { $old = $old."Approval for Cheque: No, "; }
+                                }
+                            }
+                            echo rtrim($old, ', ');
+                        }
                     @endphp
                 </td>
                 <td>
@@ -106,6 +153,47 @@
                                 else if($key == "mr_suffix") {$new = $new."Suffix: ".$value.', ';}
                                 else if($key == "mr_prefix") {$new = $new."Prefix: ".$value.', ';}
                                 else if($key == "mr_start_from") {$new = $new."MR Start From: ".$value.', ';}
+                            }
+                            echo rtrim($new, ', ');
+                        }
+
+                        if($audit->auditable_type == "App\Printer"){
+                            $new_value = json_decode($audit->new_values);
+                            $new = "";
+                            foreach($new_value as $key => $value){
+                                if($key == "print_name") {$new = $new."Name: ".$value.', ';}
+                                else if($key == "top") {$new = $new."Top: ".$value.', ';}
+                                else if($key == "left") {$new = $new."Left: ".$value.', ';}
+                                else if($key == "rotate") {$new = $new."Rotate: ".$value.', ';}
+                            }
+                            echo rtrim($new, ', ');
+                        }
+
+                        if($audit->auditable_type == "App\Setting"){
+                            $new_value = json_decode($audit->new_values);
+                            $new = "";
+                            foreach($new_value as $key => $value){
+                                if($key == "mr_number") {
+                                    if($value == "auto") { $new = $new."MR Number: Automatic, "; }
+                                    else { $new = $new."MR Number: Manual, "; }
+                                }
+                                else if($key == "mr_size") {
+                                    if($value == "full_size") { $new = $new."MR Size: Full Page, "; }
+                                    else { $new = $new."MR Size: Half Page, "; }
+                                }
+                                else if($key == "amount_in_word_format") {
+                                    if($value == "crore_lakh_thousand") { $new = $new."Amount in Word Fomrat: Crore Lakh Thousand, "; }
+                                    else if($value == "crore_lac_thousand") { $new = $new."Amount in Word Fomrat: Crore Lac Thousand, "; }
+                                    else if($value == "billion_million_thousand") { $new = $new."Amount in Word Fomrat: Billion Million Thousand, "; }
+                                }
+                                else if($key == "approval_for_mr") {
+                                    if($value == 1) { $new = $new."Approval for MR: Yes, "; }
+                                    else { $new = $new."Approval for MR: No, "; }
+                                }
+                                else if($key == "approval_for_cheque") {
+                                    if($value == 1) { $new = $new."Approval for Cheque: Yes, "; }
+                                    else { $new = $new."Approval for Cheque: No, "; }
+                                }
                             }
                             echo rtrim($new, ', ');
                         }
