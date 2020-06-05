6e82d06610e28dcb2131de46a964850098f4524c
Mostafa Emon <mostafaemon.info@gmail.com>
2020-06-05 14:56:20 +0600

    Remove meta tags

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,30 +5,6 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-        <!-- Twitter -->
-        <meta name="twitter:site" content="@themepixels">
-        <meta name="twitter:creator" content="@themepixels">
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:title" content="Bracket">
-        <meta name="twitter:description" content="Premium Quality and Responsive UI for Dashboard.">
-        <meta name="twitter:image" content="../../../../themepixels.me/bracket/img/bracket-social.html">
-
-        <!-- Facebook -->
-        <meta property="og:url" content="http://themepixels.me/bracket">
-        <meta property="og:title" content="Bracket">
-        <meta property="og:description" content="Premium Quality and Responsive UI for Dashboard.">
-
-        <meta property="og:image" content="../../../../themepixels.me/bracket/img/bracket-social.html">
-        <meta property="og:image:secure_url" content="../../../../themepixels.me/bracket/img/bracket-social.html">
-        <meta property="og:image:type" content="image/png">
-        <meta property="og:image:width" content="1200">
-        <meta property="og:image:height" content="600">
-
-        <!-- Meta -->
-        <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
-        <meta name="author" content="ThemePixels">
-
         <title>Axis Cheque & MR</title>
         <link rel="icon" href="{{asset('img/favicon.png')}}">
 
