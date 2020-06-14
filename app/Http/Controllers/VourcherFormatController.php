e192efef96f9660d97984de76e49ceb835ecb32a
Mostafa Emon <mostafaemon.info@gmail.com>
2020-06-14 20:53:42 +0600

    123545456698456

--- app/Http/Controllers/VourcherFormatController.php
+++ app/Http/Controllers/VourcherFormatController.php
@@ -32,69 +32,69 @@ class VourcherFormatController extends Controller
             $voucher_format->title                  = $request->title;
             $voucher_format->type                   = $request->type;
 
-            $voucher_format->qb_logo_top               = $request->qb_logo_top;
-            $voucher_format->qb_logo_left               = $request->qb_logo_left;
-            $voucher_format->voucher_no_top               = $request->voucher_no_top;
-            $voucher_format->voucher_no_left               = $request->voucher_no_left;
-            $voucher_format->voucher_date_top               = $request->voucher_date_top;
-            $voucher_format->voucher_date_left               = $request->voucher_date_left;
+            $voucher_format->qb_logo_top            = $request->qb_logo_top;
+            $voucher_format->qb_logo_left           = $request->qb_logo_left;
+            $voucher_format->voucher_no_top         = $request->voucher_no_top;
+            $voucher_format->voucher_no_left        = $request->voucher_no_left;
+            $voucher_format->voucher_date_top       = $request->voucher_date_top;
+            $voucher_format->voucher_date_left      = $request->voucher_date_left;
 
             if($request->payee_name == 1) {
-                $voucher_format->payee_name                    = 1;
-            }else { $voucher_format->payee_name                = 0; }
+                $voucher_format->payee_name         = 1;
+            }else { $voucher_format->payee_name     = 0; }
             
-            $voucher_format->payee_name_top                    = $request->payee_name_top;
-            $voucher_format->payee_name_left                   = $request->payee_name_left;
+            $voucher_format->payee_name_top         = $request->payee_name_top;
+            $voucher_format->payee_name_left        = $request->payee_name_left;
 
             if($request->cheque_no == 1) {
-                $voucher_format->cheque_no                    = 1;
-            }else { $voucher_format->cheque_no                = 0; }
+                $voucher_format->cheque_no          = 1;
+            }else { $voucher_format->cheque_no      = 0; }
             
-            $voucher_format->cheque_no_top                    = $request->cheque_no_top;
-            $voucher_format->cheque_no_left                   = $request->cheque_no_left;
+            $voucher_format->cheque_no_top          = $request->cheque_no_top;
+            $voucher_format->cheque_no_left         = $request->cheque_no_left;
 
             if($request->cheque_date == 1) {
-                $voucher_format->cheque_date                    = 1;
-            }else { $voucher_format->cheque_date                = 0; }
+                $voucher_format->cheque_date        = 1;
+            }else { $voucher_format->cheque_date    = 0; }
             
-            $voucher_format->cheque_date_top                    = $request->cheque_date_top;
-            $voucher_format->cheque_date_left                   = $request->cheque_date_left;
+            $voucher_format->cheque_date_top        = $request->cheque_date_top;
+            $voucher_format->cheque_date_left       = $request->cheque_date_left;
 
             if($request->received_from == 1) {
-                $voucher_format->received_from                    = 1;
-            }else { $voucher_format->received_from                = 0; }
+                $voucher_format->received_from      = 1;
+            }else { $voucher_format->received_from  = 0; }
             
-            $voucher_format->received_from_top                    = $request->received_from_top;
-            $voucher_format->received_from_left                   = $request->received_from_left;
+            $voucher_format->received_from_top      = $request->received_from_top;
+            $voucher_format->received_from_left     = $request->received_from_left;
 
             if($request->account_code == 1) {
-                $voucher_format->account_code                    = 1;
-            }else { $voucher_format->account_code                = 0; }
+                $voucher_format->account_code       = 1;
+            }else { $voucher_format->account_code   = 0; }
 
             if($request->customer_job == 1) {
-                $voucher_format->customer_job                    = 1;
-            }else { $voucher_format->customer_job                = 0; }
+                $voucher_format->customer_job       = 1;
+            }else { $voucher_format->customer_job   = 0; }
 
             if($request->class == 1) {
-                $voucher_format->class                    = 1;
-            }else { $voucher_format->class                = 0; }
+                $voucher_format->class              = 1;
+            }else { $voucher_format->class          = 0; }
 
             if($request->name == 1) {
-                $voucher_format->name                    = 1;
-            }else { $voucher_format->name                = 0; }
+                $voucher_format->name               = 1;
+            }else { $voucher_format->name           = 0; }
 
             if($request->project == 1) {
-                $voucher_format->project                    = 1;
-            }else { $voucher_format->project                = 0; }
+                $voucher_format->project            = 1;
+            }else { $voucher_format->project        = 0; }
 
             if($request->location == 1) {
-                $voucher_format->location                    = 1;
-            }else { $voucher_format->location                = 0; }
+                $voucher_format->location           = 1;
+            }else { $voucher_format->location       = 0; }
 
-            $voucher_format->table_top                    = $request->table_top;
-            $voucher_format->table_left                   = $request->table_left;
+            $voucher_format->table_top              = $request->table_top;
+            $voucher_format->table_left             = $request->table_left;
 
-            $voucher_format->signatory_top                    = $request->signatory_top;
+            $voucher_format->signatory_top          = $request->signatory_top;
             
             $voucher_format->save();
             
