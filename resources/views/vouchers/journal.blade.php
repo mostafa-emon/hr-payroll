3b6720fb84a4702a905b3deae88866765f7303b5
Mostafa Emon <mostafaemon.info@gmail.com>
2020-06-27 10:48:37 +0600

    Journal Voucher Start

--- resources/views/vouchers/journal.blade.php
+++ resources/views/vouchers/journal.blade.php
@@ -5,7 +5,7 @@
   <div class="br-pageheader pd-y-15 pd-l-20">
     <nav class="breadcrumb pd-0 mg-0 tx-12">
       <a class="breadcrumb-item" href="{{ url('/') }}">Home</a>
-      <a class="breadcrumb-item" href="{{ url('voucher-journal-voucher') }}">Journal Voucher</a>
+      <a class="breadcrumb-item" href="{{ url('tr-journal-voucher') }}">Journal Voucher</a>
     </nav>
   </div>
 
@@ -25,57 +25,76 @@
           </button>
         </div>
       @endif
+
+      <form action="{{ url('tr-journal-voucher') }}" method="POST">
+        {{ csrf_field() }}
+      <div class="row mg-b-30 b">
+
+        <div class="col-md-2">
+          <label class="tx-black tx-13">From Date</label>
+          <input type="text" id="dtpick1" name="from_date" value="{{date('d-m-Y',strtotime($from_date))}}" class="form-control" autocomplete="off"/>
+        </div>
+
+        <div class="col-md-2">
+          <label class="tx-black tx-13">To Date</label>
+          <input type="text" id="dtpick2" name="to_date" value="{{date('d-m-Y',strtotime($to_date))}}" class="form-control" autocomplete="off"/>
+        </div>
+
+        <div class="col-md-2">
+          <label class="tx-black tx-13">Amount</label>
+          <input type="text" name="amount" value="{{$amount}}" class="form-control"/>
+        </div>
+
+        <div class="col-md-2">
+          <label class="tx-black tx-13">Memo</label>
+          <input type="text" name="memo" value="{{$memo}}" class="form-control"/>
+        </div>
+
+        <div class="col-md-2" style="margin-top:28px">
+          <input type="submit" class="btn btn-primary pointer" value="Search"/>
+        </div>
+        
+      </div>
+      </form>
+
+      @if(count($data) != 0)
       <div class="bd bd-gray-300 rounded table-responsive">
         <table class="table table-striped mg-b-0">
           <thead>
             <tr>
-              <th class="text-center wd-5p">Sl</th>
-              <th class="wd-20p">Name</th>
-              <th class="text-center wd-15p">Top</th>
-              <th class="text-center wd-15p">Left</th>
-              <th class="text-center wd-15p">Rotate</th>
-              @if(roles() != "" && in_array(51, json_decode(roles(),false)))
-              <th class="text-center wd-15p">Update</th>
-              @endif
-
-              @if(roles() != "" && in_array(52, json_decode(roles(),false)))
-              <th class="text-center wd-15p">Delete</th>
-              @endif
+              <th style="width:5%" class="text-center">Sl</th>
+              <th style="width:10%">Trx Date</th>
+              <th style="width:20%">Memo</th>
+              <th style="text-align:center; width:10%">Total Amount</th>
+              <th style="width:10%;text-align:right;">Status</th>
+              <th style="width:5%;text-align:right;">Action</th>
             </tr>
           </thead>
           <tbody>
-            {{--@foreach($printers as $printer)
+              @foreach($data as $dt)
               <tr>
-                <td class="text-center">{{ $loop->iteration }}</td>
-                <td>{{ $printer->print_name }}</td>
-                <td class="text-center">{{ $printer->top }}</td>
-                <td class="text-center">{{ $printer->left }}</td>
-                <td class="text-center">{{ $printer->rotate }}</td>
-                @if(roles() != "" && in_array(51, json_decode(roles(),false)))
-                  <td class="text-center">
-                    <a class="btn btn-info btn-sm" href="{{url ('printer/update/'.$printer->id) }}"><i class= "fa fa-edit"></i> Update </a>
-                  </td>
-                @endif
-                @if(roles() != "" && in_array(52, json_decode(roles(),false)))
-                  <td class="text-center">
-                    <a class="btn btn-danger btn-sm" href="javascript:void(0)" onclick="confirmDelete({{$printer->id}})"><i class= "fa fa-minus-circle"></i> Delete</a>
-                  </td>
-                @endif
+                <td>{{$loop->iteration}}</td>
+                <td>{{ date('d-M-Y',strtotime($dt['TxnDate']))}}</td>
+                <td>{{$dt['Memo']}}</td>
+                <td style="text-align:right;">{!! number_formatting($dt['TotalAmt']) !!}</td>
+                <td style="text-align:right;">
+                  @php $is_printed = is_voucher_printed('Contra-Voucher','',$dt['Id']); @endphp
+                  @if($is_printed > 0)
+                    <span class="badge badge-success">Printed</span>
+                  @endif
+                </td>
+                <td style="text-align:right;">
+                  @php
+                    if($is_printed > 0) {$printStatus = 'printed';} else{$printStatus = 'new';}
+                  @endphp
+                  <a href="{{url('contra-voucher-preview/'.$printStatus.'/'.'na'.'/'.$dt['Id'])}}" class="btn btn-primary btn-sm pointer" style="color:white">Print</a>
+                </td>
               </tr>
-            @endforeach--}}
+              @endforeach
           </tbody>
         </table>
-      </div><br>
+      </div>
+      @endif
     </div>
   </div>
-
-  <script>
-    function confirmDelete(id){
-      var result = confirm("Are you confirm to delete?");
-      if (result) {
-          window.location = 'printer/delete/'+id
-      }
-    }
-  </script>
-
 @endsection
