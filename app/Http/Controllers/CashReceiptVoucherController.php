c68375951f59c3df4df86a82f32f0058acb0c585
Mostafa Emon <mostafaemon.info@gmail.com>
2020-07-02 09:49:11 +0600

    Cash Receipt Voucher Complete

--- app/Http/Controllers/CashReceiptVoucherController.php
+++ app/Http/Controllers/CashReceiptVoucherController.php
@@ -11,6 +11,7 @@ use App\Voucher;
 use App\Currency;
 use DateTime;
 use App\Company;
+use App\VoucherDetail;
 
 class CashReceiptVoucherController extends Controller
 {
@@ -386,7 +387,13 @@ class CashReceiptVoucherController extends Controller
 
     public function preview($print_status,$api_type,$id){
         $voucher_type = "Cash-Receipt-Voucher";
-        $data = $this->cash_receipt_voucher_print($api_type,$id);
+        if($print_status == 'printed') {
+            $data = Voucher::where('type','Cash-Receipt-Voucher')->where('api_type',$api_type)->where('document_id',$id)->first();
+            $data['transactions'] = VoucherDetail::where('voucher_id',$data->id)->get();
+        }
+        else {
+            $data = $this->cash_receipt_voucher_print($api_type,$id);
+        }
         
         $company = Company::where('id',Auth::user()->company_id)->first();
         $token = getToken();
