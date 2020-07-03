aa69fd66fd454fcbbfd428ef9181f2fbea94ca3a
Mostafa Emon <mostafaemon.info@gmail.com>
2020-07-03 12:28:27 +0600

    Prblm

--- resources/views/email/mr_full.blade.php
+++ resources/views/email/mr_full.blade.php
@@ -1,5 +1,6 @@
 <style>
-    body { margin: 0px; size: A4;}
+    @page { margin: 0px;}
+    body { margin: 0px; size: A4; font-family: Arial;}
 </style>
 
 <div id="printArea">
@@ -26,20 +27,20 @@
             </div>
         </div>
     
-        <div style="float: right; padding-bottom: 10px;">
+        <div style="float: right;">
             <div>
-            <div style="font-weight: bold; padding-right: 160px;">Money Receipt No</div>
-            <div style="float: right; border: 1px solid; height: 22px; width: 47%; margin-top: -24px; text-align: center; padding-top: 4px;">{{ $setting->mr_prefix }}{{ $transaction->invoice_no }}{{ $setting->mr_suffix }}</div>
-            </div><br><br>
+            	<div style="font-weight: bold; padding-right: 160px;">Money Receipt No</div>
+            	<div style="border: 1px solid; height: 22px; width: 150px; margin-top: -29px; text-align: center; padding-top: 4px;margin-left: 150px">hi</div>
+            	</div><br>
             <div>
-            <div style="font-weight: bold; padding-right: 160px;">Money Receipt Date</div>
-            <div style="float: right; border: 1px solid; height: 22px; width: 47%; margin-top: -24px; text-align: center; padding-top: 4px;">{{ date('d-m-Y',strtotime($transaction->created_at))}}
-            </div>
-            </div><br><br>
-            <div>
-            <div style="font-weight: bold; padding-right: 160px;">Payment Method</div>
-            <div style="float: right; border: 1px solid; height: 22px; width: 47%; margin-top: -24px; text-align: center; padding-top: 4px;">{{$transaction->payment_method}}
-            </div>
+            	<div style="font-weight: bold; padding-right: 160px;">Money Receipt Date</div>
+            	<div style="border: 1px solid; height: 22px; width: 150px; margin-top: -29px; text-align: center; padding-top: 4px;margin-left: 150px">hello
+            	</div>
+            </div><br>
+            	<div>
+            	<div style="font-weight: bold; padding-right: 160px;">Payment Method</div>
+            	<div style="border: 1px solid; height: 22px; width: 150px; margin-top: -29px; text-align: center; padding-top: 4px;margin-left: 150px">Bkash
+            	</div>
             </div>
         </div>
         
@@ -137,33 +138,6 @@
                     <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;">&nbsp;</td>
                 </tr>
 
-                <tr>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;">&nbsp;</td>
-                </tr>
-
-                <tr>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;">&nbsp;</td>
-                </tr>
-
-                <tr>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;"></td>
-                    <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-size:14px;">&nbsp;</td>
-                </tr>
-        
                 <tr style="border: 1px solid black;padding-top:5px;padding-bottom:5px;">
                 <td colspan="5" style="text-align: right;padding-top:5px;padding-bottom:5px;padding-right:10px;font-weight:bold;">Total</td>
                 <td style="text-align: center; border: 1px solid black;padding-top:5px;padding-bottom:5px;font-weight:bold;text-align:right;padding-right: 5px">{{$transaction->amount}}</td>
@@ -172,47 +146,49 @@
             </table>
         </div>
     
-        <div style="margin-top: 45px;">
+        <div style="margin-top: 25px;">
             <div style="float: left;">Amount in Word</div>
             <div style="float: right; width:81%;">{{$transaction->amount_in_word}}</div>
             <br>
-            <div style="float: left; padding-left: 138px; margin-top: -13px;">_________________________________________________________________
+            <div style="float: left; padding-left: 129px; margin-top: -13px;">___________________________________________________________________
             </div>
         </div>
         <br>
     
-        <div style="margin-top: 30px;">
+        <div style="margin-top: 10px;">
             <div style="float: left;">Purpose
             </div>
             <div style="float: left; padding-left: 76px;">{{$transaction->purpose}}
             </div>
             <br>
-            <div style="float: left; padding-left: 136px; margin-top: -13px;">_________________________________________________________________
+            <div style="float: left; padding-left: 128px; margin-top: -13px;">___________________________________________________________________
             </div>
         </div>
         <br>
     
-        <div style="margin-top: 100px; float: left; margin-bottom: 80px;">
+        <div style="margin-top: 35px; float: left;">
             <div style="text-align: center;">
             </div>
-            <br>
             <div style="margin-top: -32px;">__________________
             </div><br>
             <div style="margin-left: 37px; margin-top: -16px;">Received By
             </div>
         </div>
     
-        <div style="margin-top: 100px; float: right; margin-bottom: 80px;">
+        <div style="margin-top: 35px; float: right;">
             <div style="text-align: center;">
             </div>
-            <br>
             <div style="margin-top: -32px;">______________________
             </div><br>
             <div style="margin-left: 50px; margin-top: -16px;">Authorized By
             </div>
         </div>
+        <br>
+        <br>
+        <br>
+        <br>
 
-        <div style="font-size:12px;float:left;width:100%;text-align:center;margin-top:-75px;">
+        <div style="font-size:12px;float:left;width:100%;text-align:center;margin-top: 35px;">
             This is a System Generated Money Receipt. No Signatory is Required.
         </div>
     </div>
