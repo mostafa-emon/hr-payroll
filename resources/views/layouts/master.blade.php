1a5108bb098dafced528e7843dc31763aa266001
Mostafa <mostafaifty2@gmail.com>
2020-10-23 01:23:41 +0600

    User Menu

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -104,15 +104,19 @@
                             </ul>
 						</li>
                         <li class="slide
-                            {{ (request()->is('employee*')) ? 'is-expanded' : '' }}">
+                            {{ (request()->is('employee*')) ? 'is-expanded' : '' }}
+							{{ (!request()->is('user')) && (request()->is('user*')) ? 'is-expanded' : '' }}">
                             
-                            <a class="side-menu__item {{ (request()->is('employee*')) ? 'active' : '' }}" data-toggle="slide" href="javascript:void(0)">
+                            <a class="side-menu__item {{ (request()->is('employee*')) || (request()->is('user*')) ? 'active' : '' }}" data-toggle="slide" href="javascript:void(0)">
                                 <svg class="side-menu__icon" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 5H5v14h14V5zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z" opacity=".3"/><path d="M3 5v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2zm2 0h14v14H5V5zm2 5h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z"/></svg>
                                 <span class="side-menu__label">Employee Setup</span><i class="angle fe fe-chevron-down"></i>
                             </a>
                             
                             <ul class="slide-menu">
 								<li><a class="slide-item" href="{{url('employee')}}">Employee</a></li>
+								<li class="{{ (request()->is('user*')) ? 'active' : '' }}">
+									<a class="slide-item {{ (request()->is('user*')) ? 'active' : '' }}" href="{{url('user')}}">Users</a>
+								</li>
                             </ul>
                         </li>
 						<li class="slide">
