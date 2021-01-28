54f6a050aee77555d760ee7f0efb5000b399f941
Mostafa <mostafaifty2@gmail.com>
2021-01-29 02:43:30 +0600

    Company Img Sent With Payslip

--- resources/views/transactions/payroll/salary_sheet/email/pay_slip.blade.php
+++ resources/views/transactions/payroll/salary_sheet/email/pay_slip.blade.php
@@ -24,7 +24,10 @@
     </div>
 
     <div style="float:right;padding-right:30px;padding-top:-100px;">
-        <span style="font-family: Arial;font-size:25px;font-weight:bold;">Logo</span>
+        <span style="font-family: Arial;font-size:25px;font-weight:bold;">
+            Logo
+            {{--<img src="{{ asset('storage/'.$company_info->logo) }}" height="80" width="80"/>--}}
+        </span>
     </div>
 
     <br>
