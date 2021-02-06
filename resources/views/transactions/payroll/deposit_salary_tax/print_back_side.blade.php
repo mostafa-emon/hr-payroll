595aadd095d4fe1976304ceff9a0a9f367fcf840
Mostafa <mostafaifty2@gmail.com>
2021-02-06 20:57:13 +0600

    BackSide Modififying

--- resources/views/transactions/payroll/deposit_salary_tax/print_back_side.blade.php
+++ resources/views/transactions/payroll/deposit_salary_tax/print_back_side.blade.php
@@ -1,22 +1,24 @@
 <div id="printArea">
-    <table style="width: 100%;border-collapse: collapse;">
-        <tr>
-            <th style="border: 1px solid black;text-align:center;">SL</th>
-            <th style="border: 1px solid black;text-align:left;">Employee ID</th>
-            <th style="border: 1px solid black;text-align:left;">Name</th>
-            <th style="border: 1px solid black;text-align:left;">Month</th>
-            <th style="border: 1px solid black;text-align:right;">Amount</th>
-        </tr>
-        @foreach($employment_infos as $employee)
-        <tr>
-            <td style="border: 1px solid black;text-align:center;">{{$loop->iteration}}</td>
-            <td style="border: 1px solid black;text-align:left;">{{$employee->original_employee_id}}</td>
-            <td style="border: 1px solid black;text-align:left;">{{$employee->name}}</td>
-            <td style="border: 1px solid black;text-align:left;">{{$employee->month}} {{$employee->year}}</td>
-            <td style="border: 1px solid black;text-align:right;">{{$employee->amount}}</td>
-        </tr>
-        @endforeach
-    </table>
+    <div style="font-family: Arial;padding:5px;font-side:13px;">
+        <table style="width: 100%;border-collapse: collapse;">
+            <tr>
+                <th style="border: 1px solid black;text-align:center;padding:5px;">SL</th>
+                <th style="border: 1px solid black;text-align:left;padding:5px;">Employee ID</th>
+                <th style="border: 1px solid black;text-align:left;padding:5px;">Name</th>
+                <th style="border: 1px solid black;text-align:left;padding:5px;">Month</th>
+                <th style="border: 1px solid black;text-align:left;padding:5px;">Amount</th>
+            </tr>
+            @foreach($employment_infos as $employee)
+            <tr>
+                <td style="border: 1px solid black;text-align:center;padding:5px;">{{$loop->iteration}}</td>
+                <td style="border: 1px solid black;text-align:left;padding:5px;">{{$employee->original_employee_id}}</td>
+                <td style="border: 1px solid black;text-align:left;padding:5px;">{{$employee->name}}</td>
+                <td style="border: 1px solid black;text-align:left;padding:5px;">{{$employee->month}} {{$employee->year}}</td>
+                <td style="border: 1px solid black;text-align:right;padding:5px;">{{$employee->amount}}</td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </div>
 
 <script>
