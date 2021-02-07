1aad76b89dfd7ba92142ecbdc1bcd42ea06efa80
Mostafa <mostafaifty2@gmail.com>
2021-02-07 22:51:07 +0600

    Bank Account  Route Hide

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -99,7 +99,7 @@
 								<li><a class="slide-item" href="{{url('projects')}}">Project</a></li>
 								<li><a class="slide-item" href="{{url('branches')}}">Branch</a></li>
 								<li><a class="slide-item" href="{{url('currencies')}}">Currency</a></li>
-								<li><a class="slide-item" href="{{url('bank-accounts')}}">Bank Account</a></li>
+								<li style="display:none;"><a class="slide-item" href="{{url('bank-accounts')}}">Bank Account</a></li>
 								<li style="display:none;"><a class="slide-item" href="{{url('device-setup')}}">Device Setup</a></li>
                             </ul>
 						</li>
