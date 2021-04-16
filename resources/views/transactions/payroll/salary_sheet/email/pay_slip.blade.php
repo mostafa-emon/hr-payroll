414602a2a96a6ca8ea2b5e0bdd3000add07b5cac
Mostafa Emon <mostafaemon.info@gmail.com>
2021-04-16 19:21:40 +0600

    asset keep as it is for live test

--- resources/views/transactions/payroll/salary_sheet/email/pay_slip.blade.php
+++ resources/views/transactions/payroll/salary_sheet/email/pay_slip.blade.php
@@ -25,7 +25,7 @@
 
     <div style="float:right;padding-right:30px;padding-top:-100px;">
         <span style="font-family: Arial;font-size:25px;font-weight:bold;">
-            <img src="https://i.imgur.com/MPNREgb.png" height="80" width="80"/>
+            <img src="{{ asset('storage/'.$company_info->logo) }}" height="80" width="80"/>
         </span>
     </div>
 
